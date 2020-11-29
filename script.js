6d1a1f826937920b326388e5e3a5c6e6d448a1a1
Vianney Kabamba <vianneyk@hotmail.co.uk>
2020-11-29 19:15:55 +0000

    script file amended

--- script.js
+++ script.js
@@ -9,3 +9,52 @@ $(document).ready(function () {
     });
 
     
+
+    var history = JSON.parse(localStorage.getItem("history")) || [];
+
+    if (history.length > 0) {
+        weatherFunction(history[history.length - 1]);
+    }
+    for (var i = 0; i < history.length; i++) {
+        createRow(history[i]);
+    }
+
+    function createRow(text) {
+        var listItem = $("<li>").addClass("list-group-item").text(text);
+        $(".history").append(listItem);
+    }
+    
+    $(".history").on("click", "li", function () {
+        weatherFunction($(this).text());
+        weatherForecast($(this).text());
+    });
+
+    function weatherFunction(searchTerm) {
+
+        $.ajax({
+            
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=7bc814b27d27dfe348e17287919d852f&units=metric",
+            method: "GET",
+
+        }).then(function (data) {
+            if (history.indexOf(searchTerm) === -1) {
+                history.push(searchTerm);
+                localStorage.setItem("history", JSON.stringify(history));
+                createRow(searchTerm);
+            }
+            $("#today").empty();
+
+            var title = $("<h3>").addClass("card-title").text(data.name + " (" + new Date().toLocaleDateString() + ")");
+            var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+
+
+            var card = $("<div>").addClass("card");
+            var cardBody = $("<div>").addClass("card-body");
+            var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + " MPH");
+            var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+            var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " °C");
+
+            var lon = data.coord.lon;
+            var lat = data.coord.lat;
+
+            
